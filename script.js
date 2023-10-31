2c6fc8aa379959f629b5cad4d25bea161e7bc070
sabyrkalys <sabyralievkalys@gmail.com>
2023-10-31 17:08:54 +1000

    new file gitignor

---
+++ script.js
@@ -0,0 +1,98 @@
+// типы данных js
+Number
+String
+null
+undefined
+Boolean
+BigInt // 2* 10^53-1 
+Object 
+Symbol
+
+
+// обозначение переменных
+
+const x = 'Николай'; // string
+let g = 5;
+g = 'vsjd';
+
+function foo() {
+    let y = 6;
+}
+
+var t = true;
+console.log(t);
+
+
+function jy() {
+
+}
+
+// математические операции
+ let a = 5 * 5;
+ let b = (10 == '10'); // не строгое равенство // true
+ let р = (10 === '10'); // строгое равенство // false
+
+ //=! // не равно
+
+// Логические операции
+if(x < b){
+
+};
+
+
+if (p<b) {
+    
+} else if(p<a) {
+    
+};
+
+// switch(x) {
+    //     case 'val1' // x === val1
+    
+    
+    //     case 'val2'
+    // }
+    
+    if (p>a) {
+    
+    }else{
+    
+    };
+
+    let nub = (p>a)? "vsjdf" : 456;
+
+
+    const array = [a, b, 43, 56, true] // одномерный массив
+    const array1 = [a, b, 43, [vsdfv, vsklf, [jfslvfdk,vjsldfv,vkslfdv,],]] ;// одномерный массив;
+    const array2 = [
+        45,
+        'jvlsdf',
+        true,
+        obj = {
+            kay: value,
+            kay: value
+        }
+        
+    ] 
+
+
+    // циклы
+    let j = 'sjldfvsdf';
+    j.split()
+    for(let i=0;i>j.length; i++) {
+        let count = i
+        console.log(count)
+    }
+
+    const num = [1,2,3,5,35, 34]
+    let i = 0
+    while (i < num.length) {
+        0,1,2,3,4,5
+       let newNum =  i * num[i]
+    }
+    
+
+
+    
+    
+
